382e7907bfecf7c456ac5fa9997b2b0b143d684c
abraham241 <huguesmboundou@gmail.com>
2024-01-12 15:00:24 +0100

    section partenaire V3

--- src/Screen/Landing.jsx
+++ src/Screen/Landing.jsx
@@ -53,10 +53,10 @@ export default function Landing() {
       {/*=============== fin de la 1er section =================== */}
 
       {/* =================== section qui sommes nous =============== */}
-      <div className="flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row ">
-        <div className="w-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2 pt-10 px-10">
+      <div className="flex flex-col gap-5 sm:flex-col md:flex-col lg:flex-row xl:flex-row py-10 px-10">
+        <div className="w-full sm:w-full lg:w-1/2 xl:w-1/2 pt-10 ">
           <h1 className="text-5xl font-bold  pb-5">Qui sommes nous ?</h1>
-          <div>
+          <div className="">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
               ultrices ullamcorper imperdiet. Sed vehicula vestibulum sapien, in
@@ -126,16 +126,16 @@ export default function Landing() {
       {/* ==================== section nous choisir =========================== */}
       <section className="flex-col flex gap-10 md:flex-row lg:flex-row xl:flex-row 2xl:flex-row py-10 px-10">
         <div className=" md:flex justify-start items-center">
-          <div className="hidden lg:block xl:block md:w-1/3 lg:w-1/3 xl:w-1/3  text-4xl font-bold">
+          <div className="hidden lg:block xl:block md:w-1/3 lg:w-1/3 xl:w-1/3 text-4xl font-bold">
             POURQUOI <br /> NOUS <br /> CHOISIR ?
           </div>
-          <div className=" md:block lg:hidden xl:hidden text-4xl font-bold">
+          <div className=" md:block lg:hidden xl:hidden text-5xl font-bold">
             POURQUOI NOUS <br /> CHOISIR ?
           </div>
         </div>
         <div>
           <div className="flex-col flex gap-5 justify-center items-center md:flex-row lg:flex-row xl:flex-row 2xl:flex-row">
-            <div className="w-[380px]">
+            <div className="w-[380px] flex flex-col justify-center items-center">
               <h1 className="text-9xl font-bold">01</h1>
               <p className="text-xl text-red-600 font-bold pt-2">
                 Petit budget de transport
@@ -150,7 +150,7 @@ export default function Landing() {
             </div>
           </div>
           <div className="flex-col flex gap-5 justify-center items-center md:flex-row lg:flex-row xl:flex-row 2xl:flex-row">
-            <div className="w-[380px]">
+            <div className="w-[380px] flex flex-col justify-center items-center">
               <h1 className="text-9xl font-bold">02</h1>
               <p className="text-xl text-red-600 font-bold pt-2">
                 Pas de stress
@@ -165,7 +165,7 @@ export default function Landing() {
             </div>
           </div>
           <div className="flex-col flex gap-5 justify-center items-center md:flex-row lg:flex-row xl:flex-row 2xl:flex-row">
-            <div className="w-[380px]">
+            <div className="w-[380px] flex flex-col justify-center items-center">
               <h1 className="text-9xl font-bold">03</h1>
               <p className="text-xl text-red-600 font-bold pt-2">
                 Fiabilité & securité
@@ -184,12 +184,12 @@ export default function Landing() {
       {/* ==================== section nous choisir =========================== */}
 
       {/* ==================== section nous choisir =========================== */}
-      <section className="flex flex-col justify-center items-center gap-10 sm:flex-col md:flex-row lg:flex-row xl:flex-row py-5 px-10">
+      <section className="flex flex-col justify-between gap-10 sm:flex-col md:flex-row lg:flex-row xl:flex-row py-5 px-10">
         <div className=" ">
           <img src={paiement} alt="" />
         </div>
         <div className="  md:w-1/2 lg:w-1/2 xl:w-1/2">
-          <h1 className="md:text-8xl lg:text-8xl xl:text-8xl font-bold text-5xl ">
+          <h1 className="md:text-5xl lg:text-8xl xl:text-8xl font-bold text-5xl ">
             Facilitez vos <br /> paiements <br /> avec E-billing
           </h1>
           <div className="flex justify-start  gap-10 pt-4 w-full">
@@ -211,7 +211,7 @@ export default function Landing() {
       {/* ==================== section partenaire =========================== */}
       <section className="flex-col flex justify-between gap-5 md:flex-row  px-10 py-10 ">
         <div className=" flex  flex-col md:w-1/2">
-          <h1 className="font-bold text-5xl md:text-8xl">Devenez  notre <br /> partenaire</h1>
+          <h1 className="font-bold text-5xl md:text-5xl lg:text-8xl xl:text-8xl">Devenez  notre <br /> partenaire</h1>
           <p className="py-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere hic commodi quisquam. Rem natus voluptatem exercitationem fuga, perspiciatis nemo. Architecto fugiat quas fuga voluptates doloribus consequatur quasi ratione vero excepturi.</p>
           <button className="bg-red-600 rounded-md text-white font-bold mt-10 h-16 w-36 ">
             Postulez Ici
